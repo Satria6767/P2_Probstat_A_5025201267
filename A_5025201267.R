88375f149667cd1f20bb1f1dba4ce4f241590d87
Satria6767 <102576678+Satria6767@users.noreply.github.com>
2022-05-29 21:51:26 +0700

    Update A_5025201267.R

--- A_5025201267.R
+++ A_5025201267.R
@@ -50,24 +50,25 @@ zsum.test(mean.x = 23500, sigma.x = 3900, n.x = 100,
           alternative = "greater", mu = 20000,
           conf.level = 0.95)
 
-#5a
+#2a
 # Setuju, karena kesimpulan dari uji z menolak H0,
 # sehingga mobil dikemudikan rata-rata lebih dari
 # 20000 kilometer per tahun
 
-# 5b 
+#2b 
 # Output dari z test adalah, hipotesis alternatif
 # alternative hypothesis: true mean is greater than 20000
 # atau H1 diterima sehingga klaim benar. 
 
-#5c 
+#2c 
 # P-value dari uji tes z adalah 2.2e-16 atau mendekati 0,
 # dari hasil p-value tersebut hipotesis awal dapat ditolak
 # dan H1 diterima.
 
 #nomor 3
 #3a
-cat("H0 : mu = mu0","\n","mu !=(tidak sama dengan) mu0")
+# H0 = (miu1 = miu2)
+# H1 = (miu1 != miu2) 
 
 #3b
 tsum.test(mean.x=3.64, s.x = 1.67, n.x = 19, 
@@ -76,65 +77,48 @@ tsum.test(mean.x=3.64, s.x = 1.67, n.x = 19,
           conf.level = 0.95)
 
 #3c
-xbar3 = 2.79
-mu03 = 3.64
-s3 = 1.32
-n3 = 27              
-t3 = (xbar3-mu03)/(s3/sqrt(n3)) 
-t3  
+plotDist(dist ='t', df = 2, col="pink")
 
 #3d nilai kritis
-alpha3 = 0.05 
-t.alpha3 = qt(1-alpha3, df=2) 
-t.alpha3 
+qt(p = 0.05, df = 2, lower.tail = FALSE)
 
 #3e
-cat("Keputusan : Gagal Tolak H0")
+cat("Karena p-value < a , Hipotesis awal ditolak")
 
 #3f
-cat("Kesimpulan : Tidak ada perbedaan pada rata-rata jumlah saham perusahaan di dua kota tersebut")
+cat("Dengan tingkat keyakinan 95%, diyakini bahwa tidak terdapat perbedaan rata-rata saham pada perusahaan di Bandung dan Bali.")
 
 # NOMOR 4
-dataoneway <- read.table("onewayanova.txt",h=T)
-attach(dataoneway)
-names(dataoneway)
+#4a Pembagian menjadi 3 subjek grup dan membuat plot kuantil normal setiap kelompok
 
-dataoneway$Group <- as.factor(dataoneway$Group)
-dataoneway$Group = factor(dataoneway$Group,labels = c("Grup 1", "Grup 2", "Grup 3"))
+my_data <- read.delim(file.choose())
 
-class(dataoneway$Group)
+my_data$Group <- as.factor(my_data$Group)
+my_data$Group = factor(my_data$Group, labels = c("grup1", "grup1", "grup3"))
 
-#4a Pembagian menjadi 3 subjek grup dan membuat
-# plot kuantil normal setiap kelompok
 
-Group1 <- subset(dataoneway, Group == "Grup 1")
-Group2 <- subset(dataoneway, Group == "Grup 2")
-Group3 <- subset(dataoneway, Group == "Grup 3")
+grup1 <- subset(my_data, Group == "grup1")
+grup2 <- subset(my_data, Group == "grup1")
+grup3 <- subset(my_data, Group == "grup3")
 
-qqnorm(Group1$Length)
-qqline(Group1$Length)
+qqnorm(grup1$Length)
 
-qqnorm(Group2$Length)
-qqline(Group2$Length)
+qqnorm(grup2$Length)
 
-qqnorm(Group3$Length)
-qqline(Group3$Length)
+qqnorm(grup3$Length)
 
 #4b
-# Mencari homogenity of variances
-bartlett.test(Length ~ Group, data = dataoneway)
+bartlett.test(Length ~ Group, data = my_data)
 
 #4c 
-# Uji anova satu arah
-model1 = lm(Length ~ Group, data = dataoneway)
-anova(model1)
+model1 <- aov(Length ~ Group, data = my_data)
+summary(model1)
 
 #4d 
-# nilai p adalah 0.8054, maka H0 ditolak
+# nilai p adalah 0.0013, maka H0 ditolak
 
 #4e
-# Post-hoc test Tukey HSD
-TukeyHSD(aov(model1))
+TukeyHSD(model1)
 
 #4f
 # Visualisasikan data dengan ggplot2
